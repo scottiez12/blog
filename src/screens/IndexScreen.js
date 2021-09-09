65ab9ebc50521add5e7b2fc7050ae473e539c541
sziegler <sziegler@hcnetwork.org>
2021-09-09 14:31:49 -0400

    About halfway through this section

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,74 @@
+import React, { useContext } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { Context as BlogContext } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
+
+const IndexScreen = ({ navigation }) => {
+  const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
+  return (
+    <View>
+      <Button title="Add Post" onPress={() => addBlogPost()} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Show", { id: item.id })}
+            >
+              <View style={styles.row}>
+                <Text style={styles.title}>
+                  {item.title} - {item.id}
+                </Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather style={styles.icon} name="trash" />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+        <Feather
+          style={{ marginRight: 15 }}
+          name="plus"
+          size={40}
+          color="black"
+        />
+      </TouchableOpacity>
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 15,
+    borderTopWidth: 1,
+    borderColor: "gray",
+    paddingHorizontal: 12,
+  },
+  title: {
+    fontSize: 20,
+  },
+  icon: {
+    fontSize: 30,
+  },
+});
+
+export default IndexScreen;
